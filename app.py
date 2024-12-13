209fc58808371bdf4752c08dd26d4851b8760d55
Madeline Miller <madelinemiller723@gmail.com>
2024-12-12 17:38:06 -0800

    now works on my computer

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # This allows all origins by default
 
-
 @app.route('/recommend', methods=['GET'])
 def recommend_books():
     title = request.args.get('title', default=None, type=str)
